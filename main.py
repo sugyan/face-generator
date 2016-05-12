518537a64baf20160b72c2c5a7a3e157eb684a61
sugyan <sugi1982@gmail.com>
2016-05-13 02:43:05 +0900

    refactoring

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ import tensorflow as tf
 FLAGS = tf.app.flags.FLAGS
 tf.app.flags.DEFINE_string('train_dir', 'train',
                            """Directory where to write event logs and checkpoint.""")
+tf.app.flags.DEFINE_string('images_dir', 'images',
+                           """Directory where to write generated images.""")
 tf.app.flags.DEFINE_string('data_dir', 'data',
                            """Path to the TFRecord data directory.""")
 tf.app.flags.DEFINE_integer('num_examples_per_epoch_for_train', 10000,
@@ -17,82 +19,76 @@ tf.app.flags.DEFINE_integer('max_steps', 5000,
 
 INPUT_IMAGE_SIZE = 112
 
-class Generator:
-    def __init__(self, batch_size, f_size, depth1=1024, depth2=512, depth3=256, depth4=128):
-        self.batch_size = batch_size
-        self.f_size = f_size
-        self.depths = [depth1, depth2, depth3, depth4, 3]
-        self.z_dim = 100
-        self.z = tf.placeholder(tf.float32, [None, self.z_dim], name='z')
-        self.output = self.model(self.z, reuse=None)
-
-    def model(self, z, reuse=True):
-        i_depth = self.depths[0:4]
-        o_depth = self.depths[1:5]
-
-        with tf.variable_scope('g', reuse=reuse):
-            with tf.variable_scope('reshape'):
-                w0 = tf.get_variable('weights', [self.z_dim, i_depth[0] * self.f_size * self.f_size], tf.float32, tf.truncated_normal_initializer(stddev=0.02))
-                b0 = tf.get_variable('biases', [i_depth[0]], tf.float32, tf.zeros_initializer)
-                dc0 = tf.nn.bias_add(tf.reshape(tf.matmul(z, w0), [-1, self.f_size, self.f_size, i_depth[0]]), b0)
-                mean0, variance0 = tf.nn.moments(dc0, [0, 1, 2])
-                bn0 = tf.nn.batch_normalization(dc0, mean0, variance0, None, None, 1e-5)
-                out = tf.nn.relu(bn0)
-
-            # deconvolution layers
-            for i in range(4):
-                with tf.variable_scope('conv%d' % (i + 1)):
-                    w = tf.get_variable('weights', [5, 5, o_depth[i], i_depth[i]], tf.float32, tf.truncated_normal_initializer(stddev=0.02))
-                    b = tf.get_variable('biases', [o_depth[i]], tf.float32, tf.zeros_initializer)
-                    dc = tf.nn.conv2d_transpose(out, w, [self.batch_size, self.f_size * 2 ** (i + 1), self.f_size * 2 ** (i + 1), o_depth[i]], [1, 2, 2, 1])
-                    out = tf.nn.bias_add(dc, b)
-                    if i < 3:
-                        mean, variance = tf.nn.moments(out, [0, 1, 2])
-                        out = tf.nn.relu(tf.nn.batch_normalization(out, mean, variance, None, None, 1e-5))
-
-        return tf.nn.tanh(out)
-
-class Discriminator:
-    def __init__(self, f_size, depth1=64, depth2=128, depth3=256, depth4=512):
-        self.depths = [3, depth1, depth2, depth3, depth4]
-        self.model(tf.placeholder(tf.float32, [None, f_size * 2 ** 4, f_size * 2 ** 4, 3]), reuse=None)
-
-    def model(self, images, reuse=True):
-        i_depth = self.depths[0:4]
-        o_depth = self.depths[1:5]
-
-        with tf.variable_scope('d', reuse=reuse):
-            out = images
-            for i in range(4):
-                with tf.variable_scope('conv%d' % i):
-                    w = tf.get_variable('weights', [5, 5, i_depth[i], o_depth[i]], tf.float32, tf.truncated_normal_initializer(stddev=0.02))
-                    b = tf.get_variable('biases', [o_depth[i]], tf.float32, tf.zeros_initializer)
-                    c = tf.nn.bias_add(tf.nn.conv2d(out, w, [1, 2, 2, 1], padding='SAME'), b)
-                    mean, variance = tf.nn.moments(c, [0, 1, 2])
-                    bn = tf.nn.batch_normalization(c, mean, variance, None, None, 1e-5)
-                    out = tf.maximum(0.2 * bn, bn)
-
-            with tf.variable_scope('classify'):
-                dim = 1
-                for d in out.get_shape()[1:].as_list():
-                    dim *= d
-                w = tf.get_variable('weights', [dim, 2], tf.float32, tf.truncated_normal_initializer(stddev=0.02))
-                b = tf.get_variable('biases', [2], tf.float32, tf.zeros_initializer)
-                out = tf.nn.bias_add(tf.matmul(tf.reshape(out, [-1, dim]), w), b)
-
-        return out
-
 class DCGAN:
     def __init__(self, batch_size=128, f_size=4):
-        self.batch_size = 64
-        self.f_size = 6
-        self.g = Generator(self.batch_size, self.f_size, *[250, 150, 90, 54])
-        self.d = Discriminator(self.f_size, *[54, 90, 150, 250])
+        self.batch_size = batch_size
+        self.f_size = f_size
+        self.z_dim = 100
 
-    def train(self, inputs):
-        logits_from_i = self.d.model(inputs)
-        logits_from_g = self.d.model(self.g.output)
+        self.g = self.__generator(*(250, 150, 90, 54))
+        self.d = self.__discriminator(*(54, 90, 150, 250))
+        self.z = tf.placeholder(tf.float32, [None, self.z_dim], name='z')
 
+    def __generator(self, depth1=1024, depth2=512, depth3=256, depth4=128):
+        def model(inputs):
+            depths = [depth1, depth2, depth3, depth4, 3]
+            i_depth = depths[0:4]
+            o_depth = depths[1:5]
+            with tf.variable_scope('g', reuse=model.reuse):
+                # reshape from inputs
+                with tf.variable_scope('reshape'):
+                    w0 = tf.get_variable('weights', [self.z_dim, i_depth[0] * self.f_size * self.f_size], tf.float32, tf.truncated_normal_initializer(stddev=0.02))
+                    b0 = tf.get_variable('biases', [i_depth[0]], tf.float32, tf.zeros_initializer)
+                    dc0 = tf.nn.bias_add(tf.reshape(tf.matmul(inputs, w0), [-1, self.f_size, self.f_size, i_depth[0]]), b0)
+                    mean0, variance0 = tf.nn.moments(dc0, [0, 1, 2])
+                    bn0 = tf.nn.batch_normalization(dc0, mean0, variance0, None, None, 1e-5)
+                    out = tf.nn.relu(bn0)
+                # deconvolution layers
+                for i in range(4):
+                    with tf.variable_scope('conv%d' % (i + 1)):
+                        w = tf.get_variable('weights', [5, 5, o_depth[i], i_depth[i]], tf.float32, tf.truncated_normal_initializer(stddev=0.02))
+                        b = tf.get_variable('biases', [o_depth[i]], tf.float32, tf.zeros_initializer)
+                        dc = tf.nn.conv2d_transpose(out, w, [self.batch_size, self.f_size * 2 ** (i + 1), self.f_size * 2 ** (i + 1), o_depth[i]], [1, 2, 2, 1])
+                        out = tf.nn.bias_add(dc, b)
+                        if i < 3:
+                            mean, variance = tf.nn.moments(out, [0, 1, 2])
+                            out = tf.nn.relu(tf.nn.batch_normalization(out, mean, variance, None, None, 1e-5))
+            model.reuse = True
+            return tf.nn.tanh(out)
+        model.reuse = False
+        return model
+
+    def __discriminator(self, depth1=64, depth2=128, depth3=256, depth4=512):
+        def model(inputs):
+            depths = [3, depth1, depth2, depth3, depth4]
+            i_depth = depths[0:4]
+            o_depth = depths[1:5]
+            with tf.variable_scope('d', reuse=model.reuse):
+                outputs = inputs
+                # convolution layer
+                for i in range(4):
+                    with tf.variable_scope('conv%d' % i):
+                        w = tf.get_variable('weights', [5, 5, i_depth[i], o_depth[i]], tf.float32, tf.truncated_normal_initializer(stddev=0.02))
+                        b = tf.get_variable('biases', [o_depth[i]], tf.float32, tf.zeros_initializer)
+                        c = tf.nn.bias_add(tf.nn.conv2d(outputs, w, [1, 2, 2, 1], padding='SAME'), b)
+                        mean, variance = tf.nn.moments(c, [0, 1, 2])
+                        bn = tf.nn.batch_normalization(c, mean, variance, None, None, 1e-5)
+                        outputs = tf.maximum(0.2 * bn, bn)
+                # reshepe and fully connect to 2 classes
+                with tf.variable_scope('classify'):
+                    dim = 1
+                    for d in outputs.get_shape()[1:].as_list():
+                        dim *= d
+                    w = tf.get_variable('weights', [dim, 2], tf.float32, tf.truncated_normal_initializer(stddev=0.02))
+                    b = tf.get_variable('biases', [2], tf.float32, tf.zeros_initializer)
+            model.reuse = True
+            return tf.nn.bias_add(tf.matmul(tf.reshape(outputs, [-1, dim]), w), b)
+        model.reuse = False
+        return model
+
+    def train(self, input_images):
+        logits_from_g = self.d(self.g(self.z))
+        logits_from_i = self.d(input_images)
         for v in tf.trainable_variables():
             if 'weights' in v.name:
                 if v.name.startswith('g'):
@@ -124,14 +120,14 @@ class DCGAN:
         min_queue_examples = FLAGS.num_examples_per_epoch_for_train
         images = tf.train.shuffle_batch(
             [image],
-            batch_size=self.g.batch_size,
+            batch_size=self.batch_size,
             capacity=min_queue_examples + 3 * self.batch_size,
             min_after_dequeue=min_queue_examples
         )
         return tf.sub(tf.div(tf.image.resize_images(images, self.f_size * 2 ** 4, self.f_size * 2 ** 4), 127.5), 1.0)
 
     def generate_images(self, row=8, col=8):
-        images = tf.cast(tf.mul(tf.add(self.g.output, 1.0), 127.5), tf.uint8)
+        images = tf.cast(tf.mul(tf.add(self.g(self.z), 1.0), 127.5), tf.uint8)
         images = [tf.squeeze(image, [0]) for image in tf.split(0, self.batch_size, images)]
         rows = []
         for i in range(row):
@@ -145,39 +141,46 @@ def main(argv=None):
     train_op, g_loss, d_loss = dcgan.train(inputs)
     images = dcgan.generate_images()
 
-    g_saver = tf.train.Saver([v for v in tf.trainable_variables() if v.name.startswith('g')])
-    d_saver = tf.train.Saver([v for v in tf.trainable_variables() if v.name.startswith('d')])
+    g_variables = [v for v in tf.trainable_variables() if v.name.startswith('g')]
+    d_variables = [v for v in tf.trainable_variables() if v.name.startswith('d')]
+    g_saver = tf.train.Saver(g_variables)
+    d_saver = tf.train.Saver(d_variables)
     g_checkpoint_path = os.path.join(FLAGS.train_dir, 'g.ckpt')
     d_checkpoint_path = os.path.join(FLAGS.train_dir, 'd.ckpt')
     with tf.Session() as sess:
         # restore or initialize
+        sess.run(tf.initialize_all_variables())
         if os.path.exists(g_checkpoint_path):
+            print 'restore variables:'
+            for v in g_variables:
+                print '  ' + v.name
             g_saver.restore(sess, g_checkpoint_path)
         if os.path.exists(d_checkpoint_path):
+            print 'restore variables:'
+            for v in d_variables:
+                print '  ' + v.name
             d_saver.restore(sess, d_checkpoint_path)
-        for v in tf.all_variables():
-            if not sess.run(tf.is_variable_initialized(v)):
-                print 'initialize variable %s' % v.name
-                sess.run(tf.initialize_variables([v]))
 
+        # start training
         tf.train.start_queue_runners(sess=sess)
 
         for step in range(FLAGS.max_steps):
-            random = np.random.uniform(-1, 1, size=(dcgan.batch_size, dcgan.g.z_dim))
+            random = np.random.uniform(-1, 1, size=(dcgan.batch_size, dcgan.z_dim))
+
             start_time = time.time()
-            _, g_loss_value, d_loss_value = sess.run([train_op, g_loss, d_loss], feed_dict={dcgan.g.z: random})
+            _, g_loss_value, d_loss_value = sess.run([train_op, g_loss, d_loss], feed_dict={dcgan.z: random})
             duration = time.time() - start_time
             format_str = '%s: step %d, loss = (G: %.8f, D: %.8f) (%.3f sec/batch)'
             print format_str % (datetime.now(), step, g_loss_value, d_loss_value, duration)
 
-            # generated images
-            with open('out.png', 'wb') as f:
-                f.write(sess.run(images, feed_dict={dcgan.g.z: random}))
-
-            # save variables
-            if step % 10 == 0:
-                g_saver.save(sess, g_checkpoint_path)
+            # save generated images
             if step % 100 == 0:
+                filename = os.path.join(FLAGS.images_dir, '%04d.png' % step)
+                with open(filename, 'wb') as f:
+                    f.write(sess.run(images, feed_dict={dcgan.z: random}))
+            # save variables
+            if step % 100 == 0:
+                g_saver.save(sess, g_checkpoint_path)
                 d_saver.save(sess, d_checkpoint_path)
 
 if __name__ == '__main__':
