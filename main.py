da5fed54add5ac32c0a16093afae3ff8ae56ce71
sugyan <sugi1982@gmail.com>
2016-05-12 21:15:08 +0900

    wip

--- main.py
+++ main.py
@@ -18,127 +18,87 @@ tf.app.flags.DEFINE_integer('max_steps', 5000,
 INPUT_IMAGE_SIZE = 112
 
 class Generator:
-    def __init__(self, batch_size):
-        self.z_dim = 100
+    def __init__(self, batch_size, f_size, depth1=1024, depth2=512, depth3=256, depth4=128):
         self.batch_size = batch_size
+        self.f_size = f_size
+        self.depths = [depth1, depth2, depth3, depth4, 3]
+        self.z_dim = 100
         self.z = tf.placeholder(tf.float32, [None, self.z_dim], name='z')
         self.output = self.model(self.z, reuse=None)
 
     def model(self, z, reuse=True):
+        i_depth = self.depths[0:4]
+        o_depth = self.depths[1:5]
+
         with tf.variable_scope('g', reuse=reuse):
-            with tf.variable_scope('conv1'):
-                w1 = tf.get_variable('weights', [self.z_dim, 512 * 6 * 6], tf.float32, tf.truncated_normal_initializer(stddev=0.02))
-                b1 = tf.get_variable('biases', [512], tf.float32, tf.zeros_initializer)
-                dc1 = tf.nn.bias_add(tf.reshape(tf.matmul(z, w1), [-1, 6, 6, 512]), b1)
-                mean1, variance1 = tf.nn.moments(dc1, [0, 1, 2])
-                bn1 = tf.nn.batch_normalization(dc1, mean1, variance1, None, None, 1e-5)
-                relu1 = tf.nn.relu(bn1)
-                tf.scalar_summary(relu1.op.name + '/sparsity', tf.nn.zero_fraction(relu1))
-                tf.add_to_collection('g_losses', tf.mul(tf.nn.l2_loss(w1), 0.00001))
+            with tf.variable_scope('reshape'):
+                w0 = tf.get_variable('weights', [self.z_dim, i_depth[0] * self.f_size * self.f_size], tf.float32, tf.truncated_normal_initializer(stddev=0.02))
+                b0 = tf.get_variable('biases', [i_depth[0]], tf.float32, tf.zeros_initializer)
+                dc0 = tf.nn.bias_add(tf.reshape(tf.matmul(z, w0), [-1, self.f_size, self.f_size, i_depth[0]]), b0)
+                mean0, variance0 = tf.nn.moments(dc0, [0, 1, 2])
+                bn0 = tf.nn.batch_normalization(dc0, mean0, variance0, None, None, 1e-5)
+                out = tf.nn.relu(bn0)
 
-            with tf.variable_scope('conv2'):
-                w2 = tf.get_variable('weights', [5, 5, 256, 512], tf.float32, tf.truncated_normal_initializer(stddev=0.02))
-                b2 = tf.get_variable('biases', [256], tf.float32, tf.zeros_initializer)
-                dc2 = tf.nn.bias_add(tf.nn.conv2d_transpose(relu1, w2, [self.batch_size, 12, 12, 256], [1, 2, 2, 1]), b2)
-                mean2, variance2 = tf.nn.moments(dc2, [0, 1, 2])
-                bn2 = tf.nn.batch_normalization(dc2, mean2, variance2, None, None, 1e-5)
-                relu2 = tf.nn.relu(bn2)
-                tf.scalar_summary(relu2.op.name + '/sparsity', tf.nn.zero_fraction(relu2))
-                tf.add_to_collection('g_losses', tf.mul(tf.nn.l2_loss(w2), 0.00001))
+            # deconvolution layers
+            for i in range(4):
+                with tf.variable_scope('conv%d' % (i + 1)):
+                    w = tf.get_variable('weights', [5, 5, o_depth[i], i_depth[i]], tf.float32, tf.truncated_normal_initializer(stddev=0.02))
+                    b = tf.get_variable('biases', [o_depth[i]], tf.float32, tf.zeros_initializer)
+                    dc = tf.nn.conv2d_transpose(out, w, [self.batch_size, self.f_size * 2 ** (i + 1), self.f_size * 2 ** (i + 1), o_depth[i]], [1, 2, 2, 1])
+                    out = tf.nn.bias_add(dc, b)
+                    if i < 3:
+                        mean, variance = tf.nn.moments(out, [0, 1, 2])
+                        out = tf.nn.relu(tf.nn.batch_normalization(out, mean, variance, None, None, 1e-5))
 
-            with tf.variable_scope('conv3'):
-                w3 = tf.get_variable('weights', [5, 5, 128, 256], tf.float32, tf.truncated_normal_initializer(stddev=0.02))
-                b3 = tf.get_variable('biases', [128], tf.float32, tf.zeros_initializer)
-                dc3 = tf.nn.bias_add(tf.nn.conv2d_transpose(relu2, w3, [self.batch_size, 24, 24, 128], [1, 2, 2, 1]), b3)
-                mean3, variance3 = tf.nn.moments(dc3, [0, 1, 2])
-                bn3 = tf.nn.batch_normalization(dc3, mean3, variance3, None, None, 1e-5)
-                relu3 = tf.nn.relu(bn3)
-                tf.scalar_summary(relu3.op.name + '/sparsity', tf.nn.zero_fraction(relu3))
-                tf.add_to_collection('g_losses', tf.mul(tf.nn.l2_loss(w3), 0.00001))
-
-            with tf.variable_scope('conv4'):
-                w4 = tf.get_variable('weights', [5, 5, 64, 128], tf.float32, tf.truncated_normal_initializer(stddev=0.02))
-                b4 = tf.get_variable('biases', [64], tf.float32, tf.zeros_initializer)
-                dc4 = tf.nn.bias_add(tf.nn.conv2d_transpose(relu3, w4, [self.batch_size, 48, 48, 64], [1, 2, 2, 1]), b4)
-                mean4, variance4 = tf.nn.moments(dc4, [0, 1, 2])
-                bn4 = tf.nn.batch_normalization(dc4, mean4, variance4, None, None, 1e-5)
-                relu4 = tf.nn.relu(bn4)
-                tf.scalar_summary(relu4.op.name + '/sparsity', tf.nn.zero_fraction(relu4))
-                tf.add_to_collection('g_losses', tf.mul(tf.nn.l2_loss(w4), 0.00001))
-
-            with tf.variable_scope('output'):
-                w5 = tf.get_variable('weights', [5, 5, 3, 64], tf.float32, tf.truncated_normal_initializer(stddev=0.02))
-                b5 = tf.get_variable('biases', [3], tf.float32, tf.zeros_initializer)
-                dc5 = tf.nn.bias_add(tf.nn.conv2d_transpose(relu4, w5, [self.batch_size, 96, 96, 3], [1, 2, 2, 1]), b5)
-                out = tf.nn.tanh(dc5)
-
-        return out
+        return tf.nn.tanh(out)
 
 class Discriminator:
-    def __init__(self):
-        self.model(tf.placeholder(tf.float32, [None, 96, 96, 3]), reuse=None)
+    def __init__(self, f_size, depth1=64, depth2=128, depth3=256, depth4=512):
+        self.depths = [3, depth1, depth2, depth3, depth4]
+        self.model(tf.placeholder(tf.float32, [None, f_size * 2 ** 4, f_size * 2 ** 4, 3]), reuse=None)
 
     def model(self, images, reuse=True):
+        i_depth = self.depths[0:4]
+        o_depth = self.depths[1:5]
+
         with tf.variable_scope('d', reuse=reuse):
-            with tf.variable_scope('conv1'):
-                w1 = tf.get_variable('weights', [5, 5, 3, 64], tf.float32, tf.truncated_normal_initializer(stddev=0.02))
-                b1 = tf.get_variable('biases', [64], tf.float32, tf.zeros_initializer)
-                c1 = tf.nn.bias_add(tf.nn.conv2d(images, w1, [1, 2, 2, 1], padding='SAME'), b1)
-                mean1, variance1 = tf.nn.moments(c1, [0, 1, 2])
-                bn1 = tf.nn.batch_normalization(c1, mean1, variance1, None, None, 1e-5)
-                lrelu1 = tf.maximum(0.2 * bn1, bn1)
-                if not reuse:
-                    tf.add_to_collection('d_losses', tf.mul(tf.nn.l2_loss(w1), 0.00001))
+            out = images
+            for i in range(4):
+                with tf.variable_scope('conv%d' % i):
+                    w = tf.get_variable('weights', [5, 5, i_depth[i], o_depth[i]], tf.float32, tf.truncated_normal_initializer(stddev=0.02))
+                    b = tf.get_variable('biases', [o_depth[i]], tf.float32, tf.zeros_initializer)
+                    c = tf.nn.bias_add(tf.nn.conv2d(out, w, [1, 2, 2, 1], padding='SAME'), b)
+                    mean, variance = tf.nn.moments(c, [0, 1, 2])
+                    bn = tf.nn.batch_normalization(c, mean, variance, None, None, 1e-5)
+                    out = tf.maximum(0.2 * bn, bn)
 
-            with tf.variable_scope('conv2'):
-                w2 = tf.get_variable('weights', [5, 5, 64, 128], tf.float32, tf.truncated_normal_initializer(stddev=0.02))
-                b2 = tf.get_variable('biases', [128], tf.float32, tf.zeros_initializer)
-                c2 = tf.nn.bias_add(tf.nn.conv2d(lrelu1, w2, [1, 2, 2, 1], padding='SAME'), b2)
-                mean2, variance2 = tf.nn.moments(c2, [0, 1, 2])
-                bn2 = tf.nn.batch_normalization(c2, mean2, variance2, None, None, 1e-5)
-                lrelu2 = tf.maximum(0.2 * bn2, bn2)
-                if not reuse:
-                    tf.add_to_collection('d_losses', tf.mul(tf.nn.l2_loss(w2), 0.00001))
-
-            with tf.variable_scope('conv3'):
-                w3 = tf.get_variable('weights', [5, 5, 128, 256], tf.float32, tf.truncated_normal_initializer(stddev=0.02))
-                b3 = tf.get_variable('biases', [256], tf.float32, tf.zeros_initializer)
-                c3 = tf.nn.bias_add(tf.nn.conv2d(lrelu2, w3, [1, 2, 2, 1], padding='SAME'), b3)
-                mean3, variance3 = tf.nn.moments(c3, [0, 1, 2])
-                bn3 = tf.nn.batch_normalization(c3, mean3, variance3, None, None, 1e-5)
-                lrelu3 = tf.maximum(0.2 * bn3, bn3)
-                if not reuse:
-                    tf.add_to_collection('d_losses', tf.mul(tf.nn.l2_loss(w3), 0.00001))
-
-            with tf.variable_scope('conv4'):
-                w4 = tf.get_variable('weights', [5, 5, 256, 512], tf.float32, tf.truncated_normal_initializer(stddev=0.02))
-                b4 = tf.get_variable('biases', [512], tf.float32, tf.zeros_initializer)
-                c4 = tf.nn.bias_add(tf.nn.conv2d(lrelu3, w4, [1, 2, 2, 1], padding='SAME'), b4)
-                mean4, variance4 = tf.nn.moments(c4, [0, 1, 2])
-                bn4 = tf.nn.batch_normalization(c4, mean4, variance4, None, None, 1e-5)
-                lrelu4 = tf.maximum(0.2 * bn4, bn4)
-                if not reuse:
-                    tf.add_to_collection('d_losses', tf.mul(tf.nn.l2_loss(w4), 0.00001))
-
-            with tf.variable_scope('output'):
+            with tf.variable_scope('classify'):
                 dim = 1
-                for d in lrelu4.get_shape()[1:].as_list():
+                for d in out.get_shape()[1:].as_list():
                     dim *= d
-                w5 = tf.get_variable('weights', [dim, 2], tf.float32, tf.truncated_normal_initializer(stddev=0.02))
-                b5 = tf.get_variable('biases', [2], tf.float32, tf.zeros_initializer)
-                out = tf.nn.bias_add(tf.matmul(tf.reshape(lrelu4, [-1, dim]), w5), b5)
+                w = tf.get_variable('weights', [dim, 2], tf.float32, tf.truncated_normal_initializer(stddev=0.02))
+                b = tf.get_variable('biases', [2], tf.float32, tf.zeros_initializer)
+                out = tf.nn.bias_add(tf.matmul(tf.reshape(out, [-1, dim]), w), b)
 
         return out
 
 class DCGAN:
-    def __init__(self):
-        self.batch_size = 128
-        self.g = Generator(self.batch_size)
-        self.d = Discriminator()
+    def __init__(self, batch_size=128, f_size=4):
+        self.batch_size = 64
+        self.f_size = 6
+        self.g = Generator(self.batch_size, self.f_size, *[250, 150, 90, 54])
+        self.d = Discriminator(self.f_size, *[54, 90, 150, 250])
 
     def train(self, inputs):
         logits_from_i = self.d.model(inputs)
         logits_from_g = self.d.model(self.g.output)
+
+        for v in tf.trainable_variables():
+            if 'weights' in v.name:
+                if v.name.startswith('g'):
+                    tf.add_to_collection('g_losses', tf.mul(tf.nn.l2_loss(v), 1e-5))
+                if v.name.startswith('d'):
+                    tf.add_to_collection('d_losses', tf.mul(tf.nn.l2_loss(v), 1e-5))
         tf.add_to_collection('g_losses', tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(logits_from_g, tf.ones([self.batch_size], dtype=tf.int64))))
         tf.add_to_collection('d_losses', tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(logits_from_i, tf.ones([self.batch_size], dtype=tf.int64))))
         tf.add_to_collection('d_losses', tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(logits_from_g, tf.zeros([self.batch_size], dtype=tf.int64))))
@@ -168,24 +128,26 @@ class DCGAN:
             capacity=min_queue_examples + 3 * self.batch_size,
             min_after_dequeue=min_queue_examples
         )
-        return tf.sub(tf.div(tf.image.resize_images(images, 96, 96), 127.5), 1.0)
+        return tf.sub(tf.div(tf.image.resize_images(images, self.f_size * 2 ** 4, self.f_size * 2 ** 4), 127.5), 1.0)
 
     def generate_images(self, num):
         images = tf.cast(tf.mul(tf.add(self.g.output, 1.0), 127.5), tf.uint8)
         return map(lambda image: tf.image.encode_png(tf.squeeze(image, [0])), tf.split(0, self.batch_size, images)[0:num])
 
 def main(argv=None):
-    dcgan = DCGAN()
+    dcgan = DCGAN(batch_size=64, f_size=6)
     inputs = dcgan.inputs([os.path.join(FLAGS.data_dir, f) for f in os.listdir(FLAGS.data_dir) if f.endswith('.tfrecords')])
     train_op, g_loss, d_loss = dcgan.train(inputs)
     images = dcgan.generate_images(9)
 
-    saver = tf.train.Saver(tf.all_variables())
+    saver = tf.train.Saver(tf.trainable_variables())
     checkpoint_path = os.path.join(FLAGS.train_dir, 'model.ckpt')
+
     with tf.Session() as sess:
         # restore or initialize
         if os.path.exists(checkpoint_path):
             saver.restore(sess, checkpoint_path)
+            sess.run(tf.initialize_variables([v for v in tf.all_variables() if not v in tf.trainable_variables()]))
         else:
             sess.run(tf.initialize_all_variables())
 
@@ -200,7 +162,7 @@ def main(argv=None):
             print format_str % (datetime.now(), step, g_loss_value, d_loss_value, duration)
 
             for i, image in enumerate(sess.run(images, feed_dict={dcgan.g.z: random})):
-                with open('%i.png' % i, 'wb') as f:
+                with open('%02d.png' % i, 'wb') as f:
                     f.write(image)
 
             if step % 10 == 0:
