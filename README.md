8cb0615ed4964a1d8c22cb9fbdeeb8a145d59e67
sugyan <sugi1982@gmail.com>
2016-05-14 00:31:43 +0900

    add `is_train` flag

--- README.md
+++ README.md
@@ -3,3 +3,16 @@
 ### DCGAN ###
 
 http://arxiv.org/pdf/1511.06434v2.pdf
+
+
+### Requirement ###
+
+- Python >= 3.5
+ - TensorFlow >= 0.8.0
+
+
+### How to generate ###
+
+    $ python3 main.py --is_train False
+
+will write generated images to `images/out.png`.
--- main.py
+++ main.py
@@ -19,6 +19,8 @@ tf.app.flags.DEFINE_integer('num_examples_per_epoch_for_train', 10000,
                             """number of examples for train""")
 tf.app.flags.DEFINE_integer('max_steps', 5000,
                             """Number of batches to run.""")
+tf.app.flags.DEFINE_boolean('is_train', True,
+                            """True for training, False for generate only""")
 
 INPUT_IMAGE_SIZE = 112
 
@@ -68,27 +70,34 @@ def main(argv=None):
                 print('  ' + v.name)
             d_saver.restore(sess, d_checkpoint_path)
 
-        # start training
-        tf.train.start_queue_runners(sess=sess)
+        if FLAGS.is_train:
+            # start training
+            tf.train.start_queue_runners(sess=sess)
 
-        for step in range(FLAGS.max_steps):
+            for step in range(FLAGS.max_steps):
+                random = np.random.uniform(-1, 1, size=(dcgan.batch_size, dcgan.z_dim))
+
+                start_time = time.time()
+                _, g_loss_value, d_loss_value = sess.run([train_op, g_loss, d_loss], feed_dict={dcgan.z: random})
+                duration = time.time() - start_time
+                format_str = '%s: step %d, loss = (G: %.8f, D: %.8f) (%.3f sec/batch)'
+                print(format_str % (datetime.now(), step, g_loss_value, d_loss_value, duration))
+
+                # save generated images
+                if step % 100 == 0:
+                    filename = os.path.join(FLAGS.images_dir, '%04d.png' % step)
+                    with open(filename, 'wb') as f:
+                        f.write(sess.run(images, feed_dict={dcgan.z: random}))
+                # save variables
+                if step % 100 == 0:
+                    g_saver.save(sess, g_checkpoint_path)
+                    d_saver.save(sess, d_checkpoint_path)
+        else:
             random = np.random.uniform(-1, 1, size=(dcgan.batch_size, dcgan.z_dim))
-
-            start_time = time.time()
-            _, g_loss_value, d_loss_value = sess.run([train_op, g_loss, d_loss], feed_dict={dcgan.z: random})
-            duration = time.time() - start_time
-            format_str = '%s: step %d, loss = (G: %.8f, D: %.8f) (%.3f sec/batch)'
-            print(format_str % (datetime.now(), step, g_loss_value, d_loss_value, duration))
-
-            # save generated images
-            if step % 100 == 0:
-                filename = os.path.join(FLAGS.images_dir, '%04d.png' % step)
-                with open(filename, 'wb') as f:
-                    f.write(sess.run(images, feed_dict={dcgan.z: random}))
-            # save variables
-            if step % 100 == 0:
-                g_saver.save(sess, g_checkpoint_path)
-                d_saver.save(sess, d_checkpoint_path)
+            generated = sess.run(images, feed_dict={dcgan.z: random})
+            filename = os.path.join(FLAGS.images_dir, 'out.png')
+            with open(filename, 'wb') as f:
+                f.write(generated)
 
 if __name__ == '__main__':
     tf.app.run()
