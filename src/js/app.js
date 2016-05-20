002828e9873092aba5cf6ce21d8a1a6377942b32
sugyan <sugi1982@gmail.com>
2016-05-20 23:55:34 +0900

    update frontend

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,14 @@
 class Main {
+    constructor(canvas) {
+        canvas.height = 96;
+        canvas.width  = 96;
+        this.ctx = canvas.getContext('2d');
+    }
     run() {
-        window.console.log(1);
+        window.console.log(this.ctx);
     }
 }
 
 window.addEventListener('DOMContentLoaded', () => {
-    new Main().run();
+    new Main(document.getElementById('canvas')).run();
 });
