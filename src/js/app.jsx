b4291335892864d6c490a0db1fd591ca9f493a71
sugyan <sugi1982@gmail.com>
2016-05-29 23:31:23 +0900

    web: morphing arrays

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -3,51 +3,55 @@
 class Faces extends React.Component {
     constructor(props) {
         super(props);
+        this.rows = 3;
+        this.cols = 8;
         this.state = {
-            faces: []
+            faces: [...Array(this.rows)].map(() => [...Array(this.cols)])
         };
     }
     componentDidMount() {
         const z_dim = 40;
-        const cols = 8;
+        const z0 = [...Array(z_dim)].map(() => Math.random() * 2 - 1);
 
-        const z0 = [];
-        const z1 = [];
-        const d = [];
-        let scale = Infinity;
-        for (let i = 0; i < z_dim; i++) {
-            z0[i] = Math.random() * 2 - 1;
-            z1[i] = Math.random() * 2 - 1;
-            d[i] = z1[i] - z0[i];
-            scale = Math.min(scale, 1.0 + ((d[i] > 0.0 ? 1.0 : -1.0) - z1[i]) / d[i]);
-        }
-        const zs = [];
-        for (let i = 0; i < cols; i++) {
-            const z = [];
+        for (let i = 0; i < this.rows; i++) {
+            const z1 = [];
+            const d = [];
+            let scale = Infinity;
             for (let j = 0; j < z_dim; j++) {
-                z[j] = z0[j] + (i + 1) / cols * d[j] * scale;
+                z1[j] = Math.random() * 2 - 1;
+                d[j] = z1[j] - z0[j];
+                scale = Math.min(scale, 1.0 + ((d[j] > 0.0 ? 1.0 : -1.0) - z1[j]) / d[j]);
             }
-            zs.push(z);
-        }
-        zs.forEach((e, i) => {
-            $.ajax({
-                url: '/api/generate',
-                method: 'POST',
-                data: JSON.stringify(e),
-                contentType: 'application/json',
-                success: (data) => {
-                    const faces = this.state.faces;
-                    faces[i] = data.result;
-                    this.setState({
-                        faces: faces
-                    });
+            const zs = [];
+            for (let j = 0; j < this.cols; j++) {
+                const z = [];
+                for (let k = 0; k < z_dim; k++) {
+                    z[k] = z0[k] + (j + 1) / this.cols * d[k] * scale;
                 }
+                zs.push(z);
+            }
+            zs.forEach((e, j) => {
+                $.ajax({
+                    url: '/api/generate',
+                    method: 'POST',
+                    data: JSON.stringify(e),
+                    contentType: 'application/json',
+                    success: (data) => {
+                        const faces = this.state.faces;
+                        faces[i][j] = data.result;
+                        this.setState({
+                            faces: faces
+                        });
+                    }
+                });
             });
-        });
+        }
     }
     render() {
-        const faces = this.state.faces.map((e, i) => {
-            return <img key={i} src={e} />;
+        const faces = this.state.faces.map((row, i) => {
+            return (
+                <div>{row.map((e, j) => <img key={`${i}:${j}`} src={e} />)}</div>
+            );
         });
         return (
             <div>{faces}</div>
