504e30a43c4f81ad5a44de119702f9fe7e0b5d21
sugyan <sugi1982@gmail.com>
2016-06-13 01:29:31 +0900

    web: update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -24,6 +24,9 @@ const config = {
 
 if (process.env.NODE_ENV === 'production') {
     config.plugins = [
+        new webpack.DefinePlugin({
+            'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+        }),
         new webpack.optimize.UglifyJsPlugin({
             compress: {
                 warnings: false
