bfddbd4adb48ae98e2d2e381b4ffc09a87ddf3cf
sugyan <sugi1982@gmail.com>
2016-06-18 22:31:22 +0900

    web: add FloatingActionButton

--- src/js/components/morphing.jsx
+++ src/js/components/morphing.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import FloatingActionButton from 'material-ui/FloatingActionButton';
+import ContentAdd from 'material-ui/svg-icons/content/add';
 
 export default class Morphing extends React.Component {
     render() {
         return (
             <div>
               <h2>Morphing</h2>
+              <FloatingActionButton>
+                <ContentAdd />
+              </FloatingActionButton>
             </div>
         );
     }
