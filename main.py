d71a138a4633f7e3034c1821b9ab90d877731928
sugyan <sugi1982@gmail.com>
2016-06-21 16:21:56 +0900

    update parameters

--- main.py
+++ main.py
@@ -46,8 +46,8 @@ def inputs(batch_size, f_size):
 def main(argv=None):
     dcgan = DCGAN(
         batch_size=96, f_size=6, z_dim=40,
-        gdepth1=512, gdepth2=256, gdepth3=128,  gdepth4=64,
-        ddepth1=54,  ddepth2=90,  ddepth3=150, ddepth4=250)
+        gdepth1=216, gdepth2=144, gdepth3=96,  gdepth4=64,
+        ddepth1=64,  ddepth2=96,  ddepth3=144, ddepth4=216)
     input_images = inputs(dcgan.batch_size, dcgan.f_size)
     train_op, g_loss, d_loss = dcgan.train(input_images, learning_rate=0.0001)
 
