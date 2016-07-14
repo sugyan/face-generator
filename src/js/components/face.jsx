19919fd83bc27914887b2e27fac6940844f671de
sugyan <sugi1982@gmail.com>
2016-07-14 18:31:17 +0900

    web: update

--- src/js/components/face.jsx
+++ src/js/components/face.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import Paper from 'material-ui/Paper';
 import CircularProgress from 'material-ui/CircularProgress';
+import FlatButton from 'material-ui/FlatButton';
 import 'whatwg-fetch';
 
 export default class Face extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = {
+            src: props.src
+        };
     }
     componentDidMount() {
         fetch('/api/generate', {
@@ -23,13 +26,21 @@ export default class Face extends React.Component {
             }, 750);
         });
     }
+    handleTouchTap() {
+        this.props.onTouchTap(this);
+    }
     render() {
         return (
-            <Paper zDepth={2} style={{ float: 'left', height: 96, width: 96, margin: 5 }}>
-              {this.state.src
-              ? <img src={this.state.src} />
-              : <CircularProgress size={1.92 / 1.4} />}
-            </Paper>
+            <FlatButton
+                style={{ height: undefined }}
+                disabled={(this.props.onTouchTap === undefined) || !this.state.src}
+                onTouchTap={this.handleTouchTap.bind(this)}>
+              <Paper zDepth={2} style={{ float: 'left', height: 96, width: 96, margin: 5 }}>
+                {this.state.src
+                 ? <img src={this.state.src} />
+                 : <CircularProgress size={1.92 / 1.4} />}
+              </Paper>
+            </FlatButton>
         );
     }
 }
--- src/js/components/morphing.jsx
+++ src/js/components/morphing.jsx
@@ -11,25 +11,39 @@ export default class Morphing extends React.Component {
         super(props);
         this.z_dim = 40;
         this.state = {
-            open: false,
-            faces: []
+            openDialog: false,
+            selectedFaces: [],
+            randomFaces: []
         };
     }
     handleClickAddButton() {
         this.setState({
-            open: true,
-            faces: Array.from(Array(25), () => {
+            openDialog: true,
+            randomFaces: Array.from(Array(25), () => {
                 return Array.from(Array(this.z_dim), () => Math.random() * 2 - 1);
             })
         });
     }
     handleClose() {
         this.setState({
-            open: false
+            openDialog: false
+        });
+    }
+    selectFace(face) {
+        const faces = this.state.selectedFaces;
+        faces.push(face);
+        this.setState({
+            openDialog: false,
+            selectedFaces: faces
         });
     }
     render() {
-        const faces = this.state.faces.map((z, i) => <Face key={i} z={z} />);
+        const randomFaces = this.state.randomFaces.map((z, i) => {
+            return <Face key={i} z={z} onTouchTap={this.selectFace.bind(this)} />;
+        });
+        const selectedFaces = this.state.selectedFaces.map((f, i) => {
+            return <Face key={i} z={f.props.z} src={f.state.src} />;
+        });
         return (
             <div>
               <h2>Morphing</h2>
@@ -37,11 +51,12 @@ export default class Morphing extends React.Component {
                   title="Select Face"
                   actions={<FlatButton label="Cancel" primary={true} onTouchTap={this.handleClose.bind(this)} />}
                   modal={true}
-                  open={this.state.open}
+                  open={this.state.openDialog}
                   autoScrollBodyContent={true}
                   contentStyle={{ maxWidth:(96 + 10) * 5 + 24 * 2 }}>
-                {faces}
+                {randomFaces}
               </Dialog>
+              <div>{selectedFaces}</div>
               <FloatingActionButton onTouchTap={this.handleClickAddButton.bind(this)}>
                 <ContentAdd />
               </FloatingActionButton>
