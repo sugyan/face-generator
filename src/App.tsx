231462ff4a4c400365bb0651c52b34da169233a0
sugyan <sugi1982@gmail.com>
2021-04-05 00:36:55 +0900

    Add tailwind
    https://tailwindcss.com/docs/guides/create-react-app

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
-function App() {
+import React from "react";
+
+const App: React.FC = () => {
   return <div className="App"></div>;
-}
+};
 
 export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ['./src/**/*.{ts,tsx}', './public/index.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
