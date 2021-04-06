48c208b3e8d3bc1909042421ded52ee10c5ee0e5
sugyan <sugi1982@gmail.com>
2021-04-07 00:27:52 +0900

    Revert "Add tailwind"

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,7 @@
 import React from "react";
 
 const App: React.FC = () => {
-  return (
-    <div className="App">
-      <div className="container mx-auto py-2">
-        <button className="py-2 px-4 font-semibold rounded shadow-md text-white bg-green-500 active:bg-green-700">
-          generate
-        </button>
-      </div>
-    </div>
-  );
+  return <div className="App"></div>;
 };
 
 export default App;
