45c6b270a033f2066a6c38d3a8b7f85021695bc8
sugyan <sugi1982@gmail.com>
2021-04-07 23:58:47 +0900

    Add bulma

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const App: React.FC = () => {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="container">
+        <button className="button is-primary">generate</button>
+      </div>
+    </div>
+  );
 };
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+import "bulma/css/bulma.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
